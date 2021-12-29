0c6aa6ea8ecf35d5a18beccd8231149aff63da24
sahil <muhammed.sahil@centresource.in>
2021-12-29 18:45:36 +0530

    second commit

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -6,4 +6,6 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController('api::home.home',()=> ({
+
+}));
